1b7bf6fe8d9ac5733d2546459ad2cefa035a1e4a
steven <zou995201314@163.com>
2021-05-06 22:48:34 +0800

    更新

--- js/leanCloud.js
+++ js/leanCloud.js
@@ -1,56 +1,73 @@
+!function(){
+  var model = {
+    init: function(){
+      var  APP_ID  = "euE2p5cOiI76a2HNil724Qqn-MdYXbMMI";
+      var  APP_KEY  = "i0Uci98oEDC5e15OH6lIFhEG";
+      AV.init({appId: APP_ID,appKey: APP_KEY,});
+    },
+    fetch: function(){
+      var query = new AV.Query('message');
+      return query.find()
+    },
+    save: function(name,content){
+      var Message = AV.Object.extend('message');
+      var message = new Message();
+      return message.save({
+        'name':name,
+        'content':content
+      })
+    }
+  }
+  var view  = document.querySelector('section.messageBox')
+
+  var controller = {
+    view: null,
+    model: null,
+    messageList: null,
+    init:function(view,model){
+      this.view = view
+      this.model  = model
+
+      this.messageList  = view.querySelector('#messageList')
+      this.form = view.querySelector('form')
+      this.model.init()
+      this.loadMessage()
+      this.bindEvents()
+    },
+    loadMessage:function(){
+      this.model.fetch().then(messages=>{
+        let array = messages.map(item=>item.attributes)
+        array.forEach(item=>{
+          let li = document.createElement('li')
+          li.innerText = `${item.name}:${item.words?item.words:item.content}`;
+          this.messageList.append(li)
+        })
+      })
+    },
+    bindEvents:function(){
+      this.form.addEventListener('submit',(e)=>{
+        e.preventDefault()
+        // console.log(this,"指向")
+        this.saveMessage()
+      })
+    },
+    saveMessage:function(){
+      let myForm  = this.form
+      let name  = myForm.querySelector('input[name =name').value
+      let content = myForm.querySelector('input[name=content]').value
+      this.model.save(name,content).then(function(object){
+        let li = document.createElement('li')
+        li.innerText = `${object.attributes.name}:${object.attributes.words?object.attributes.words:object.attributes.content}`;
+        let messagesList = document.querySelector('#messageList')
+        messageList.append(li)
+        myForm.querySelector('input[name=content]').value= ''
+      })
+    }
+  }
+  controller.init(view,model)
+}.call()
 
 
 
-var  APP_ID  = "3fcpVOyCiMnw9lHqjIutSpnP-gzGzoHsz";
-var  APP_KEY  = "Da75g8J7LWpAgXY2GwGNobKe";
-// var SERVER_URL = "http://10.10.106.155:8080/1.1/date"
 
 
-AV.init({
-    appId: "euE2p5cOiI76a2HNil724Qqn-MdYXbMMI",
-    appKey: "i0Uci98oEDC5e15OH6lIFhEG",
-    // serverURL: SERVER_URL
-  });
-
-  
-// // 数据api存储接口
-// const TestObject = AV.Object.extend('message');
-// const testObject = new TestObject();
-// testObject.set('words', 'Hello world!');
-// testObject.save().then((testObject) => {
-//   console.log('第二条数据')
-// })
-
-var query = new AV.Query('message');
-query.find().then(function(messages){
-  // 成功之后的操作
-  console.log(messages)
-  let array = messages.map(item=>item.attributes)
-  array.forEach(item=>{
-    let li = document.createElement('li')
-    li.innerText = `${item.name}:${item.words?item.words:item.content}`;
-    messageList.append(li)
-  })
-}),function(error){
-  // 异常处理
-}
-
-let  myForm  = document.querySelector('#messageForm')
-
-myForm.addEventListener('submit',function(e){
-  e.preventDefault()
-
-  let name  = myForm.querySelector('input[name =name').value
-  let content = myForm.querySelector('input[name=content]').value
-  var Message = AV.Object.extend('message');
-  var message = new Message();
-  message.save({
-    'content':content,
-    'name':name
-  }).then(function(object){
-    let li = document.createElement('li')
-    li.innerText = `${object.attributes.name}:${object.attributes.words?object.attributes.words:object.attributes.content}`;
-    messageList.append(li)
-    let content = myForm.querySelector('input[name=content]').value= ''
-  })
-})
---
+++ js/model.js
@@ -0,0 +1,20 @@
+window.Model = function(options){
+	let resourceName = options.resourceName
+	return {
+		init:function(){
+			var APP_ID = '',
+			var APP_KEY = ''
+			AV.init({appId: APP_ID, appKey:APP_KEY })
+		},
+		fetch: function(){
+			var query = new AV.Query(resourceName);
+			return query.find()
+		},
+		save: function(){
+			var x  = AV.Obeject.extend(resourceName);
+			var x  =  new x();
+			return x.save(obeject)
+		}
+	}
+}
+
